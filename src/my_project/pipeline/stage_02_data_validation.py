f9f96935236c485de74a9f629a40eb063e169a03
Muzammil Khan <m.khan@utwente.nl>
2025-09-20 19:47:57 +0200

    data validation added

---
+++ src/my_project/pipeline/stage_02_data_validation.py
@@ -0,0 +1,16 @@
+from my_project.config.configuration import ConfigurationManager
+from my_project.components.data_validation import DataValiadtion
+from my_project import logger
+
+
+STAGE_NAME = "Data Validation stage"
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValiadtion(config=data_validation_config)
+        data_validation.validate_all_columns()
