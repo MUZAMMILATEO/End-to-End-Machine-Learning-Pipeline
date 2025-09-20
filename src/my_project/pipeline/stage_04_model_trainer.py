b755404a05498c3084854585444d6b96a1600d3b
Muzammil Khan <m.khan@utwente.nl>
2025-09-20 20:43:01 +0200

    model evaluation added

---
+++ src/my_project/pipeline/stage_04_model_trainer.py
@@ -0,0 +1,17 @@
+from my_project.config.configuration import ConfigurationManager
+from my_project.components.model_trainer import ModelTrainer
+from my_project import logger
+
+
+
+STAGE_NAME = "Model Trainer stage"
+
+class ModelTrainerTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        model_trainer_config = config.get_model_trainer_config()
+        model_trainer_config = ModelTrainer(config=model_trainer_config)
+        model_trainer_config.train()
---
+++ src/my_project/pipeline/stage_05_model_evaluation.py
@@ -0,0 +1,15 @@
+from my_project.config.configuration import ConfigurationManager
+from my_project.components.model_evaluation import ModelEvaluation
+from my_project import logger
+
+STAGE_NAME = "Model evaluation stage"
+
+class ModelEvaluationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        model_evaluation_config = config.get_model_evaluation_config()
+        model_evaluation_config = ModelEvaluation(config=model_evaluation_config)
+        model_evaluation_config.save_results()
